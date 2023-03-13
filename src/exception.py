d6874d5ea1744346d2d8283f27339e6d0ab2fb0f
Ramin Nourizadeh <ramin.nourizade@gmail.com>
2023-03-13 09:38:16 +0330

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-from src.logger import mylogging
-
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -16,13 +15,14 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+       
     
     def __str__(self):
         return self.error_message
-
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as ex:
-        mylogging.info("taghsim bar sefr")
-        raise CustomException(ex,sys)
+    
+# try:
+#     a=1/0
+# except Exception as ex:
+#     logging.info("taghsim bar sefr")
+#     raise CustomException(ex,sys)

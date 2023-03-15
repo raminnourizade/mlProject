5a6cc4eb966c4705967bec267e66080c963a56c7
Ramin Nourizadeh <ramin.nourizade@gmail.com>
2023-03-15 14:29:44 +0330

    --

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,15 +4,15 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
-from sklearn.model_selecttion import train_test_split
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=sys.path.join('artifacts','train.csv')
-    test_data_path:str=sys.path.join('artifacts','test.csv')
-    raw_data_path:str=sys.path.join('artifacts','raw.csv')
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
 
 class DataIngestion:
     def __init__(self) -> None:
